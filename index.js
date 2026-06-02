84a76259435733de7efd5fc620e537dee1bcb879
Volodymyr Markovych <imarkovychi@gmail.com>
2026-06-02 20:27:26 +0200

    backend petproject #5

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+var multer = require('multer');
 
 var app = express();
+var upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +14,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.json({ error: 'No file uploaded' });
+  }
 
+  return res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
-
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
